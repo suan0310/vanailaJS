7c5ba79b8beb79b8c17785250cab23e067ea190c
suan0310 <80302803+suan0310@users.noreply.github.com>
2021-03-27 22:27:01 +0900

    Add files via upload

---
+++ Arrays.js
@@ -0,0 +1,10 @@
+// const mon = "Mon";
+// const tue = "Tue";
+// const wed = "Wed";
+// const thu = "Thu";
+// const fri = "Fri";
+// console.log(mon,tue, wed,thu, fri);
+
+const daysOfWeek = ["Mon","The","Wed","Thu","Fri","Sat","Sun"];
+console.log(daysOfWeek);
+console.log(3);
