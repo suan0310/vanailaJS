e75ecfaff389c6973ca82237a72e2667bea53b27
suan0310 <80302803+suan0310@users.noreply.github.com>
2021-03-27 22:19:30 +0900

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,22 @@
+// variable(변수)
+//const : constant(상수) its stable, doesnt changed
+// var a = 221;    var 또한 변수 
+// const a =221;
+let a = 221;
+let b =a -5;
+a=4;
+console.log(b,a);
+
+// 첫 사용은 const로, 진짜 필요할때만 let
+// String 
+// const what = "suan";
+// console.log(what);
+
+//  Boolean
+// const what = true;
+
+// Number
+// const what =666;
+
+// float 
+// what = 55.1;
