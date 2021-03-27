aa6e2a8924e9ca9485b06067f11a2c9dc8dd99d4
suan0310 <80302803+suan0310@users.noreply.github.com>
2021-03-27 22:59:59 +0900

    Add files via upload

---
+++ Function_1.js
@@ -0,0 +1,12 @@
+// console.log('Grettings baby')
+// console.log('Grettings jun')
+// console.log('Grettings Liyn')
+// console.log('Grettings hun')
+
+function sayHello(name, age ){  //함수를 외부에서 주는 방법
+    // console.log() 함수는 argument를 무한하게 가질 수 있음.타입 상관X
+    console.log('Hello!',tomato," age is ",age);
+}
+sayHello("Liyn",15);
+
+console.log("Hi!")
